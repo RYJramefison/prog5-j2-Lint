58417bcf261752ef2ba8e3cdb03493c6e10577bb
juninho <hei.juninho.2@gmail.com>
2025-11-20 10:48:52 +0300

    feat: add checkstyle

---
+++ Wallet.java
@@ -0,0 +1,23 @@
+public class Wallet {
+
+    private String color;
+    private Long taille;
+    private Long open;
+    private boolean close;
+
+    public void addVola(){
+
+    }
+
+    public void getVola(){
+
+    }
+
+    public Boolean isLost(){
+        return null;
+    }
+
+    public long checkVola(){
+        return 0;
+    }
+}
