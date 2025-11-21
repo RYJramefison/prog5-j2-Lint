6beed0a56100340e38ac5c46d669d9652776f791
juninho <hei.juninho.2@gmail.com>
2025-11-21 10:16:42 +0300

    fix: Wallet class

--- Wallet.java
+++ Wallet.java
@@ -1,23 +1,38 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class Wallet {
+    private Color color;
+    private Size size;
+    private long vola;
+    private boolean isLost;
 
-    private String color;
-    private Long taille;
-    private Long open;
-    private boolean close;
-
-    public void addVola(){
-
+    public long addVola(long amount){
+        return vola += amount;
     }
 
-    public void getVola(){
-
+    public long checkVola(long amount){
+        return vola -= amount ;
     }
 
-    public Boolean isLost(){
-        return null;
+    public void open(){
+        System.out.println("the wallet is opened");
     }
 
-    public long checkVola(){
-        return 0;
+    public void close(){
+        System.out.println("the wallet is closed");
+    }
+
+    public static void main(String[] args) {
+        var wallet = new Wallet(Color.RED, Size.MEDIUM, 1000, false);
+        System.out.println(wallet);
+        System.out.println(wallet.addVola(100));
+        System.out.println(wallet);
     }
 }
